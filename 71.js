67e17f5e12d22b7e4fce1f1e7e65918f487a97c1
jeongwooHam <hamjw0122@gmail.com>
2023-04-29 20:22:11 +0900

    0428(71번)

--- 71.js
+++ 71.js
@@ -1,5 +1,5 @@
 /*
-깊이 우선 탐색이란 목표한 노드를 찾기 위해 가장 우선순위가 높은 노드의 자식으로 깊이 들어갔다가 
+깊이 우선 탐색(Depth First Search)이란 목표한 노드를 찾기 위해 가장 우선순위가 높은 노드의 자식으로 깊이 들어갔다가 
 목표 노드가 존재하지 않으면 처음 방문한 노드와 연결된 다른 노드부터 그 자식 노드로 파고드는 검색 방법을 말합니다.
 
 다음과 같이 리스트 형태로 노드들의 연결 관계가 주어진다고 할 때 
@@ -45,13 +45,15 @@ function findChild(el) {
   }
 }
 
-for (let i = 0; i < graph.E.length; i++) {
-  // 최상위 노드의 각 요소의 자식 요소 찾기
-  //   console.log(graph[graph.E[i]]);
-  resultArr.push(graph.E[i]);
+function DFS(obj) {
+  for (let i = 0; i < obj.E.length; i++) {
+    // 최상위 노드의 각 요소의 자식 요소 찾기
+    //   console.log(graph[graph.E[i]]);
+    resultArr.push(obj.E[i]);
 
-  findChild(graph.E[i]);
+    findChild(obj.E[i]);
+  }
+  return resultArr.join(" ");
 }
-console.log(resultArr);
 
-// console.log(resultArr);
+console.log(DFS(graph));
