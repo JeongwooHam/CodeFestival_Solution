be2ec8079b6b3bd36955718d1046ce7b14c71d0e
jeongwooHam <hamjw0122@gmail.com>
2023-05-04 12:19:14 +0900

    0331 (11-15번)

---
+++ CodeFestival/13.js
@@ -0,0 +1,16 @@
+function Planet(num) {
+  const planet = [
+    "수성",
+    "금성",
+    "지구",
+    "화성",
+    "목성",
+    "토성",
+    "천왕성",
+    "해왕성",
+  ];
+
+  console.log(planet[num - 1]);
+}
+
+Planet(1);
