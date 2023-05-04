f50c1792bbdb272c9661643efeaaf6445661bdcd
jeongwooHam <hamjw0122@gmail.com>
2023-05-04 12:14:42 +0900

    0330 (1-10번)

---
+++ CodeFestival/04.js
@@ -0,0 +1,9 @@
+let a;
+
+console.log(typeof (a = 1)); //number
+
+console.log(typeof (a = 2.22)); //number
+
+console.log(typeof (a = "p")); //string
+
+console.log(typeof (a = [1, 2, 3])); //object
---
+++ CodeFestival/07.js
@@ -0,0 +1,9 @@
+let age = 1;
+
+let Age = 2;
+
+// let let = 3; >> 예약어를 사용할 수 없음
+
+let _age = 4;
+
+// let 1age = 5; >> 숫자가 앞에 올 수 없음
