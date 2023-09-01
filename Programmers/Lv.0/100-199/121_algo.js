2e21ceb9ed4260e5c743f9a1b1aa6233df51da92
jeongwooHam <hamjw0122@gmail.com>
2023-09-01 20:40:49 +0900

    0901 (121번)

--- Programmers/Lv.0/100-199/121_algo.js
+++ Programmers/Lv.0/100-199/121_algo.js
@@ -31,7 +31,6 @@ n을 3x 마을에서 사용하는 숫자로 바꿔 return하도록 solution 함�
 14 22
 15 25
 */
-// 이번에는 더 간단하게 푸는 방법들이 있었는데 생각해내지 못한 것 같다.
 
 const hasThree = (num) => {
   const toStr = num + "";
@@ -84,5 +83,35 @@ const solution = (n) => {
   }
   return num;
 };
+*/
 
+// 이번에는 더 간단하게 푸는 방법들이 있었는데 생각해내지 못한 것 같다.
+// 다른 풀이 1
+/*
+function solution(n) {
+  // 길이가 n*3인 배열 생성
+  return [...Array(n * 3)]
+  // 1부터 시작할 수 있도록 map 사용
+    .map((_, i) => i + 1)
+    // filter: 3을 포함하지 않고 3의 배수가 아닌 수만 남도록 필터링
+    // [n-1]: 필터링된 배열에서 n번째로 작은 숫자 선택 (인덱스는 0부터 시작하므로 n-1 사용)
+    .filter((num) => num % 3 !== 0 && !num.toString().includes("3"))[n - 1];
+}
+➡️ 이를 통해 주어진 조건을 만족하는 숫자들만 포함된 배열에서 n번째 숫자를 도출
+*/
+
+// 다른 풀이 2
+/*
+function solution(n) {
+    let arr = [];
+    let num = 0;
+    // arr의 길이가 n과 같지 않을 때까지 반복
+    // 💡 num 변수를 1씩 증가시키며 다음 수 검사
+    while (arr.length !== n && ++num){
+      // 3의 배수가 아니고 3을 포함하지 않는 경우만 arr에 담기
+      if (num%3!==0 && !(''+num).includes('3')) arr.push(num);}
+    // arr에는 조건에 만족하는 숫자들만 들어있음
+    ➡️ arr.pop()으로 조건에 만족하는 가장 마지막 숫자를 꺼내 반환
+    return arr.pop();
+}
 */
