2e26671a782705739e49d7848f149e3336916c47
jeongwooHam <hamjw0122@gmail.com>
2023-09-30 23:35:00 +0900

    0930 (146 풀이중)

---
+++ Programmers/Lv.0/100-199/146_algo.js
@@ -0,0 +1,47 @@
+/*
+[베스트 앨범]
+스트리밍 사이트에서 장르 별로 가장 많이 재생된 노래를 두 개씩 모아 베스트 앨범을 출시하려 합니다. 
+노래는 고유 번호로 구분하며, 노래를 수록하는 기준은 다음과 같습니다.
+
+속한 노래가 많이 재생된 장르를 먼저 수록합니다.
+장르 내에서 많이 재생된 노래를 먼저 수록합니다.
+장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다.
+노래의 장르를 나타내는 문자열 배열 genres와 노래별 재생 횟수를 나타내는 정수 배열 plays가 주어질 때, 
+베스트 앨범에 들어갈 노래의 고유 번호를 순서대로 return 하도록 solution 함수를 완성하세요.
+
+* 제한사항
+- genres[i]는 고유번호가 i인 노래의 장르입니다.
+- plays[i]는 고유번호가 i인 노래가 재생된 횟수입니다.
+- genres와 plays의 길이는 같으며, 이는 1 이상 10,000 이하입니다.
+- 장르 종류는 100개 미만입니다.
+- 장르에 속한 곡이 하나라면, 하나의 곡만 선택합니다.
+- 모든 장르는 재생된 횟수가 다릅니다.
+*/
+
+const solution = (genres, plays) => {
+  const map = new Map();
+
+  for (let i = 0; i < genres.length; i++) {
+    if (map.has(genres[i]))
+      map.set(
+        genres[i],
+        [...map.get(genres[i]), i].sort((a, b) => b - a)
+      );
+    else map.set(genres[i], [i]);
+  }
+
+  for (let [genre, idx] of map) {
+    // 속한 노래가 많이 재생된 장르
+    const playCnt = idx.reduce((sum, i) => sum + plays[i]);
+    console.log("cnt", genre, playCnt);
+  }
+
+  return map;
+};
+
+console.log(
+  solution(
+    ["classic", "pop", "classic", "classic", "pop"],
+    [500, 600, 150, 800, 2500]
+  )
+);
