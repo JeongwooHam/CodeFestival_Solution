3c449c4e724d98e58b04e7cbc27a3ca93e2036b7
JeongwooHam <123251211+JeongwooHam@users.noreply.github.com>
2023-11-28 02:47:13 +0900

    [Silver V] Title: 셀프 넘버, Time: 248 ms, Memory: 12756 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.js
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.js
@@ -1,37 +1,14 @@
-// 풀이 방법: boolean 배열 만들고 나중에 true인 값의 index + 1 출력하기
-
-const func = () => {
-  // 길이가 10000인 배열 생성하기
-  const arr = Array.from({ length: 10000 }, () => true);
-
-  const makeSelfNum = (target, i) =>
-    i + target.reduce((sum, num) => sum + num * 1, 0);
-
-  for (let i = 1; i <= arr.length; i++) {
-    let target = (i + "").split("");
-    let len = target.length;
-    // n자리 수라면
-    if (len === 1) {
-      // 홀수일 때
-      [arr[i + i - 1]] = [false];
-      if (!(i % 2)) [arr[i - 1]] = [false];
-    } else {
-      while (target.length <= 4) {
-        target = (i + "").split("");
-        target = makeSelfNum(target, i);
-        [arr[target - 1]] = [false];
-      }
-    }
-    // 10001이면 출력 X이므로 10000은 필요 없음
+const selfNumSet = new Set();
+for (let i = 1; i <= 10000; i++) {
+  let sum = 0;
+  // 문자로 만들기
+  let str = i + "";
+  // 숫자 더해주기
+  for (let k = 0; k < str.length; k++) {
+    sum += str[k] * 1;
   }
-    
-  // 배열 수정하기
-  const result = arr.map((v, i) => (v ? i + 1 : v)).filter((v) => v);
-
-  for(let i = 0; i < result.length; i++){
-      console.log(result[i])
-  }
-
-};
-
-func();
+  selfNumSet.add(i + sum);
+}
+for (let i = 1; i <= 10000; i++) {
+  if (!selfNumSet.has(i)) console.log(i);
+}
