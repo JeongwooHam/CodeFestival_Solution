6376f36d1163bbbb108b84689b53f28e23651cb3
JeongwooHam <hamjw0122@gmail.com>
2023-11-29 13:34:15 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.71 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,12 +1,13 @@
-function solution(A,B){
-    let answer = 0;
-    
-    const sortedA = A.sort((a, b) => a - b);
-    const sortedB = B.sort((a, b) => b - a);
-    
-    for(let i = 0; i < A.length; i++){
-        answer += sortedA[i] * sortedB[i]
-    }
+function solution(A, B) {
+  let answer = 0;
 
-    return answer;
+  // sort 자체가 원본을 변경하므로 변수에 할당할 필요가 없었다!
+  const sortedA = A.sort((a, b) => a - b);
+  const sortedB = B.sort((a, b) => b - a);
+
+  for (let i = 0; i < A.length; i++) {
+    answer += sortedA[i] * sortedB[i];
+  }
+
+  return answer;
 }
