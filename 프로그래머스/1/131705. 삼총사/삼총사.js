18dc2ae53d75fb40de978b2303b3b7af138cfed5
JeongwooHam <123251211+JeongwooHam@users.noreply.github.com>
2023-12-08 10:28:01 +0900

    [level 1] Title: 삼총사, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,14 @@
+const solution = (n) => {
+    let answer = 0;
+    
+    for(let i = 0; i < n.length-2; i++){
+        let sum = n[i];
+        for(let j = i+1; j < n.length-1; j++){
+            for(let k = j+1; k < n.length; k++){
+                if(sum+n[j]+n[k] === 0) answer++
+            }
+        }
+    }
+    
+    return answer
+}
